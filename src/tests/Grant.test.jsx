c220821dc499acd38274117ecdf0667c5deb4e34
Markus Maga <markus@nadilus.com>
2018-08-03 03:05:56 +0200

    Refactor grant test to not depend on other components

--- src/tests/Grant.test.jsx
+++ src/tests/Grant.test.jsx
@@ -1,67 +1,120 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import { GrantDefinitions, Grant, Can } from '../';
+import { Grant } from '../';
+import GrantContext from '../GrantContext';
 
 const A = () => 'A';
-const B = () => 'B';
-
-const Demo = () => (
-  <div>
-    <Can do="show:a"><A /></Can>
-    <Can do="show:b"><B /></Can>
-  </div>
-);
 
 describe('<Grant />', () => {
   it('provides access', () => {
-    const wrapper = mount(
-      <Grant accessTo="show:a show:c">
-        <Demo />
-      </Grant>
+    expect.assertions(3);
+
+    mount(
+      <GrantContext.Provider value={{ canDo: () => false, defined: () => true }}>
+        <Grant accessTo="show:a show:c">
+          <GrantContext.Consumer>
+            {({ canDo }) => {
+              expect(canDo('show:a')).toEqual(true);
+              expect(canDo('show:b')).toEqual(false);
+              expect(canDo('show:c')).toEqual(true);
+            }}
+          </GrantContext.Consumer>
+        </Grant>
+      </GrantContext.Provider>
     );
-
-    expect(wrapper.find(A).exists()).toEqual(true);
-    expect(wrapper.find(B).exists()).toEqual(false);
   });
 
-  it('shows a in first, and b in second', () => {
-    const wrapper = mount(
-      <div>
-        <Grant accessTo="show:a">
-          <Demo />
-        </Grant>
+  it('checks if actions are defined', () => {
+    const defined = jest.fn(() => false);
 
-        <Grant accessTo="show:b">
-          <Demo />
-        </Grant>
-      </div>
-    );
-
-    expect(wrapper.find(Grant)).toHaveLength(2);
-    expect(wrapper.find(Grant).at(0).find(A).exists()).toEqual(true);
-    expect(wrapper.find(Grant).at(0).find(B).exists()).toEqual(false);
-    expect(wrapper.find(Grant).at(1).find(A).exists()).toEqual(false);
-    expect(wrapper.find(Grant).at(1).find(B).exists()).toEqual(true);
-  });
-
-  it('Grant throws if actions is not defined', () => {
-    expect(() => mount(
-      <GrantDefinitions defined="show:a">
-        <Grant accessTo="show:a show:wtf">
-          <A />
-        </Grant>
-      </GrantDefinitions>
-    )).toThrow('Unknown action show:wtf');
-  });
-
-  it('Grant does not throw if actions are defined', () => {
-    expect(() => mount(
-      <GrantDefinitions defined="show:a show:b show:c">
+    mount(
+      <GrantContext.Provider value={{ defined }}>
         <Grant accessTo="show:a show:c">
           <A />
         </Grant>
-      </GrantDefinitions>
-    )).not.toThrow()
+      </GrantContext.Provider>
+    );
+
+    expect(defined).toHaveBeenCalledTimes(1);
+    expect(defined).toBeCalledWith('show:a show:c');
+  });
+
+  it('extends defined grants when nested', () => {
+    expect.assertions(6);
+
+    const mock = jest.fn(() => false);
+
+    mount(
+      <GrantContext.Provider value={{ canDo: mock, defined: () => true }}>
+        <Grant accessTo="show:a show:c">
+          <Grant accessTo="show:d">
+            <GrantContext.Consumer>
+              {({ canDo }) => {
+                expect(canDo('show:a')).toEqual(true);
+                expect(canDo('show:c')).toEqual(true);
+                expect(canDo('show:b')).toEqual(false);
+                expect(canDo('show:d')).toEqual(true);
+              }}
+            </GrantContext.Consumer>
+          </Grant>
+        </Grant>
+      </GrantContext.Provider>
+    );
+
+    expect(mock).toHaveBeenCalledTimes(1);
+    expect(mock).toBeCalledWith('show:b');
+  });
+
+  it('only grants access in nested scope', () => {
+    expect.assertions(2);
+
+    mount(
+      <GrantContext.Provider value={{ canDo: () => false, defined: () => true }}>
+        <Grant accessTo="show:a show:c">
+          <GrantContext.Consumer>
+            {({ canDo }) => {
+              expect(canDo('show:d')).toEqual(false);
+            }}
+          </GrantContext.Consumer>
+
+          <Grant accessTo="show:d">
+            <GrantContext.Consumer>
+              {({ canDo }) => {
+                expect(canDo('show:d')).toEqual(true);
+              }}
+            </GrantContext.Consumer>
+          </Grant>
+        </Grant>
+      </GrantContext.Provider>
+    );
+  });
+
+  it('only grants access in its own scope', () => {
+    expect.assertions(6);
+
+    mount(
+      <GrantContext.Provider value={{ canDo: () => false, defined: () => true }}>
+        <Grant accessTo="show:a show:c">
+          <GrantContext.Consumer>
+            {({ canDo }) => {
+              expect(canDo('show:a')).toEqual(true);
+              expect(canDo('show:b')).toEqual(false);
+              expect(canDo('show:c')).toEqual(true);
+            }}
+          </GrantContext.Consumer>
+        </Grant>
+
+        <Grant accessTo="show:b">
+          <GrantContext.Consumer>
+            {({ canDo }) => {
+              expect(canDo('show:a')).toEqual(false);
+              expect(canDo('show:b')).toEqual(true);
+              expect(canDo('show:c')).toEqual(false);
+            }}
+          </GrantContext.Consumer>
+        </Grant>
+      </GrantContext.Provider>
+    );
   });
 });
